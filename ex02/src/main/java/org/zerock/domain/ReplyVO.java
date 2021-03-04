25125f9c26172ef0de1a25a81e60a2310f498bc3
sorrbas <sorrbas@naver.com>
2021-03-04 15:48:15 +0900

    댓글기능연습

---
+++ ex02/src/main/java/org/zerock/domain/ReplyVO.java
@@ -0,0 +1,21 @@
+package org.zerock.domain;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class ReplyVO {
+	
+	
+	private Long rno;
+	private Long bno;
+	
+	
+	
+	private String reply;
+	private String replyer;
+	private Date replyDate;
+	private Date updateDate;
+
+}
